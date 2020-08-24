0b898388f54553040095f9e51e7d3c437d9631a6
vmabuza <vmabuza@student.wethinkcode.co.za>
2020-08-24 14:12:14 +0200

    added the Tower class

--- src/com/vmabuza/avajLauncher/Weather/Tower.java
+++ src/com/vmabuza/avajLauncher/Weather/Tower.java
@@ -1,3 +1,24 @@
 package com.vmabuza.avajLauncher.Weather;
 
-import com.vmabuza.avajLauncher.Interface.Flyable
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vmabuza.avajLauncher.Interface.Flyable;
+
+public class Tower {
+    private List<Flyable> observers = new ArrayList<Flyable>();
+
+    public void register(Flyable flyable) {
+        observers.add(flyable);
+    }
+
+    public void unregister(Flyable flyable) {
+        observers.remove(flyable);
+    }
+
+    protected void conditionsChanged() {
+        for (int i = 0; i < observers.size(); i++) {
+            observers.get(i).updateCondition();
+        }
+    }
+}
