70b5980e9194a02236d491a3bbc9ab42f9d84709
vmabuza <vmabuza@student.wethinkcode.co.za>
2020-08-26 16:33:42 +0200

    added the WeatherTower class

---
+++ src/com/vmabuza/avajLauncher/Weather/WeatherTower.java
@@ -0,0 +1,10 @@
+import com.vmabuza.avajLauncher.Aircrafts.Coordinates;
+
+public class WeatherTower extends Tower {
+    public String getWeather(Coordinates coordinates) {
+        return WeatherProvider.getProvider().getCurrentWeather(coordinates);
+    }
+    void changeWeather() {
+        conditionsChanged();
+    }
+}
