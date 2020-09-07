1611472a29cf269d3772b84db4e675f9c16b5323
vmabuza <vmabuza@student.wethinkcode.co.za>
2020-09-07 16:10:13 +0200

    It aint over yet

--- avajLauncher/src/Interface/Flyable.java
+++
@@ -1,8 +0,0 @@
-package com.vmabuza.avajLauncher.Interface;
-
-import com.vmabuza.avajLauncher.Weather.WeatherTower;
-
-public interface Flyable {
-    void updateConditions();
-    void registerTower(WeatherTower weatherTower);
-}
